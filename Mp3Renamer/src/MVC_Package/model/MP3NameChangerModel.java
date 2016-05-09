5919655795b259a8679ec39844d134885117529f
Morgan Farmer <capton_m113@yahoo.com>
2016-05-09 17:54:28 -0400

    improved the setWorkingDir Method

--- Mp3Renamer/src/MVC_Package/model/MP3NameChangerModel.java
+++ Mp3Renamer/src/MVC_Package/model/MP3NameChangerModel.java
@@ -1,10 +1,9 @@
 package MVC_Package.model;
 
 import MVC_Package.Controller.MP3NameChangerController;
+import com.sun.org.apache.xpath.internal.SourceTree;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -13,14 +12,13 @@ import java.util.Scanner;
  * Created by Morgan on 5/1/2016.
  */
 public class MP3NameChangerModel {
-
+    private static final String DIRTXT = "musicRoot.txt";
     private File workingDir;
-    private String absolutePath = new File("").getAbsolutePath(); //used to work relatively with directory
     private ArrayList<Path> mp3FilesList;
     private MP3NameChangerController controller;
 
     public MP3NameChangerModel (){
-
+        //Default constructor (never used)
     };
 
     public MP3NameChangerModel(MP3NameChangerController controller){
@@ -41,6 +39,10 @@ public class MP3NameChangerModel {
         checkForMusicLoc();
     }
 
+    /**
+     * looks for musicRoot.txt file that contains the last user defined music root file. if no file exists then
+     * it creates one and
+     */
     private void checkForMusicLoc(){
         File file = new File("musicRoot.txt");
         if (file.isFile()){
@@ -68,10 +70,25 @@ public class MP3NameChangerModel {
     }
 
     /**
-     * Takes in a file f and updates models working directory variable. Then tells the controller to update view.
+     * Takes in a file f and updates models working directory variable.
+     * Then it updates the text file with the working music directory
+     * Then tells the controller to update view(s).
      */
     public void setWorkingDir(File f){
         this.workingDir = f;
+        try{
+            //Delete old file...
+            File file = new File(DIRTXT);
+            file.delete();
+
+            //Make new file with new music file directory
+            File newFile = new File(DIRTXT);
+            PrintWriter writer = new PrintWriter(newFile);
+            writer.println(workingDir.getAbsolutePath());
+            writer.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
         controller.updateWorkingPath(workingDir);
     }
 }
